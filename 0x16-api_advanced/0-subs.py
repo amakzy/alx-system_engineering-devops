bafdc44f81d1b8b06a60f12fae971993ed1a38b6
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 05:01:22 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """
-Module for a function that returns the number of subscribers in a subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to retrieve the
-        subscriber count for.
-
-    Returns:
-        int: The number of subscribers for the subreddit, or 0 if the
-        subreddit is invalid.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'MyApp/0.0.1'}
-
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
