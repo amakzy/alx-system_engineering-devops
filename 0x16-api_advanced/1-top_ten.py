e9d0320862669cc031c0e8122420da0bc0aac63e
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 01:09:35 +0000

    aff

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,42 +1,16 @@
 #!/usr/bin/python3
-"""
-1-top_ten.py
-
-This module defines a function to
-fetch titles of the top 10 hot posts from a subreddit.
-"""
-import json
+"""print the titles"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API for the top
-    10 hot posts of a subreddit and
-    prints their titles.
+    """print the titles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    Args:
-        subreddit (str): The name of the subreddit to search.
-
-    Returns:
-        None
-    """
-
-    url = f"https://reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "My Reddit API Script v1.0"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-
-        data = response.json()
-        if "data" in data and "children" in data["data"]:
-            for post in data["data"]["children"][:10]:
-                print(post["data"]["title"])
-        else:
-            print("Subreddit not found or invalid response format.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-    except KeyError as e:
-        print(f"Invalid JSON response: Missing key {e}")
+    res = requests.get(url, headers={'User-agent': 'your bot 0.1'})
+    if res.status_code > 300:
+        print("None")
+        return
+    data = res.json().get("data")
+    for i in range(0, 10):
+        print(data.get("children")[i].get("data").get("title"))
