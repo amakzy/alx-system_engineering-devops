44d63911074c23d41537ed65630849103efe9cd7
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 01:12:31 +0000

    aff

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,42 @@
 #!/usr/bin/python3
-"""print the titles"""
-import json
+"""
+    List the top 10 hot posts
+"""
+
+
 import requests
 
 
 def top_ten(subreddit):
-    """print the titles"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """
+        returns a list of the top 10 hot posts in
+        a given subreddit
+        Args:
+        subreddit: Account to search
+    """
+    userAgent = 'Python.wsl2.windows.ApiProject:v1 (by Dry-Improvement-3814)'
 
-    res = requests.get(url, headers={'User-agent': 'your bot 0.1'})
-    if res.status_code > 300:
-        print("None")
-        return
-    data = res.json().get("data")
-    for i in range(0, 10):
-        print(data.get("children")[i].get("data").get("title"))
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+
+    url = 'https://reddit.com/r/{}/hot.json'.format(subreddit)
+
+    _headers = {
+        'User-Agent': userAgent
+    }
+
+    _params = {
+        'limit': 10
+    }
+
+    with requests.get(url, headers=_headers, params=_params) as response:
+        titles = response.json()
+        if response.status_code == 200:
+            ch = titles.get('data').get('children')
+            if ch is None or (len(ch) > 0 and ch[0].get('kind') != 't3'):
+                print('None')
+            else:
+                for c in ch:
+                    print(c.get('data').get('title'))
+        else:
+            print('None')
