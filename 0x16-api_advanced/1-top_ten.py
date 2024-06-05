ec5202e8666abc79af9cd667e9d55f7c879b725e
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 04:48:55 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-"""Contains top_ten function"""
+"""a module that defines the top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """Return the top ten reddits posts of a subreddit.
+
+    arguments:
+        subreddit -- the subreddit to be checked
+    Return: the top ten reddits posts 
+    """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    res = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
+    if res.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    results = res.json().get("data")
+    [print(x.get("data").get('title')) for x in results.get("children")]
