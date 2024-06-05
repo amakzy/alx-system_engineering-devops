40aff8c57f3ecafd2bc95f41573dc429fa77f1a1
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 04:45:32 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+import sys
 
 
-def number_of_subscribers(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except:
-        return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
