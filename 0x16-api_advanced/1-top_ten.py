ba32d5dde24aaf0885f67980c129627746ef3b49
amakzy <ahmadamakxy128@gmail.com>
2024-06-05 00:29:30 +0000

    aff

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
+
 """
-fxn that queries Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit.
+Function that queries Reddit API and prints
+the titles of the first 10 hot posts
+listed for a given subreddit.
 """
+
 import json
 import requests
 
+
 def top_ten(subreddit):
-    """Return the first 10 hot posts listed for a given subreddit."""
-    URL = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Chrome/51.0.2704.103'}
-    r = requests.get(URL, headers=headers)
-    if r.status_code == 200:
-        r = r.json()
-        get = r.get('data').get('children')
-        my_list = []
-        for x in get:
-            y = x.get("data")
-            print(y.get("title"))
+    """
+    Return the first 10 hot posts listed for a given subreddit.
+    """
+    url = "(link unavailable)".format(subreddit)
+    headers = {"User-Agent": "Chrome/51.0.2704.103"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
     else:
         print(None)
