60a628edcba2d046bc493e7dfe518a0e9605508a
amakzy <ahmadamakxy128@gmail.com>
2024-08-17 00:40:35 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-#Install flask / Version must be 2.1.0
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+  ensure  => '2.1.0',
+  provider => 'pip3',
+  requires => [ Package['werkzeug'] ],  # Make sure werkzeug is installed first
+}
+
+package { 'werkzeug':
+  ensure  => '2.1.1',  # Replace with compatible Werkzeug version
+  provider => 'pip3',
 }
