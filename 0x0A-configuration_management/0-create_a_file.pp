09d5cc9f813ea44eedec8204efaa9481e5184ea1
amakzy <ahmadamakxy128@gmail.com>
2024-08-17 00:50:16 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp directory
+# 0-create_a_file.pp
+
 file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  ensure     => 'file',
+  owner     => 'www-data',
+  group     => 'www-data',
+  mode      => '0744',
+  content   => 'I love Puppet',
 }
