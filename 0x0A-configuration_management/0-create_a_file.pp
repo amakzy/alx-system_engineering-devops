f94b6225a85d6a51762ef91729a5f35f78c5435f
ahmad <ahmadamakxy128@gmail.com>
2024-08-16 15:28:43 -0700

    asdf

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp with specific permissions and ownership
 
 file { '/tmp/school':
-  ensure     => 'file',
-  owner     => 'www-data',
-  group     => 'www-data',
-  mode      => '0744',
-  content   => 'I love Puppet',
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
